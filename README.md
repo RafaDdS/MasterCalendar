39f88fc1f966c35cfde83ad9d97b3346301e5758
RafaDdS <rafunga@gmail.com>
2025-02-25 07:23:46 -0300

    Basic data extraction
    - How to use on read me

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # MasterCalendar
  Script to automate PESC emails to Google calendar.
+
+ This is a quick project to solve a very specific problem. I want an automation to convert emails about master and doctorate presentations to events in my google calendar.
+
+ ## You will need
+- Python 3.12
+- Chrome Browser
+- pipenv
+
+ ## How to use
+  Open the main folder on a terminal and run:
+```
+  pipenv install
+  pipenv run python main.py
+```
+  A gmail login session will open on a new chrome window. Then you need to:
+  - Login with your account.
+  - It's always a good idea to close any popups that show up. They might lead to wrong results.
+  - Navigate to the email you want to process.
+  - Input "Vai" on the terminal to indicate to the script you want to process the current email.
+  - Input "Sair" on the terminal to indicate you want to halt the script. This will also close the chrome window.
--- main.py
+++ main.py
@@ -13,12 +13,36 @@ while True:
 		if command == "Vai":
 			textElement = driver.find_element(By.XPATH, "/html/body/div[7]/div[3]/div/div[2]/div[4]/div/div/div/div[2]/div/div[1]/div/div[3]/div/div[2]/div[2]/div/div[3]/div/div/div/div/div/div[1]/div[2]/div[3]/div[3]")
 			text = textElement.text
-			print(text)
-			##re.match(text, r"")
+			print(len(text))
+			
+			titleMatch = re.search(r"tulo: ([\w/() :,]+)", text)
+			linkMatch = re.search(r"Sala virtual: ([\w:/.-]+)", text)
+			roomMatch = re.search(r"Sala( presencial)*: ([\w-]+)", text)
+			DateTimeMatch = re.search(r"Data\/hora\/local:\n- (\d+\/\d+(\/\d+)?)[^\d]*(\d+:*\d*h?)", text)
+
+			title = titleMatch.group(1) if titleMatch else ""
+			link = linkMatch.group(1) if linkMatch else ""
+			room = roomMatch.group(2) if roomMatch else ""
+			date = DateTimeMatch.group(1) if DateTimeMatch else ""
+			time = DateTimeMatch.group(3) if DateTimeMatch else ""
+
+			print("Título: ", title)
+			print("Link: ", link)
+			print("Sala: ", room)
+			print("Data: ", date)
+			print("Hora: ", time)
 
 		elif command == "Sair":
 			driver.quit()
 			break
 
+		else:
+			print("Não entendi! Vai ou Sair")
+
 	except NoSuchElementException:
-		continue
+		print("Element not found")
+
+	except Exception as e:
+		print(e)
+		driver.quit()
+		break
